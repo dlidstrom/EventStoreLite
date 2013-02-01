6f5b9fae8e6d4f726b2ae30f732952f1385032ee
Daniel Lidström <dlidstrom@gmail.com>
2013-02-01 05:37:19 -0800

    Create ObjectReferenceEqualityComparer.cs

---
+++ ObjectReferenceEqualityComparer.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace RavenEventStore
+{
+    public class ObjectReferenceEqualityComparer<T> : EqualityComparer<T> where T : class
+    {
+        public static new readonly IEqualityComparer<T> Default = new ObjectReferenceEqualityComparer<T>();
+
+        public override bool Equals(T x, T y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return RuntimeHelpers.GetHashCode(obj);
+        }
+    }
+}
