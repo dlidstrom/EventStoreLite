477ec9c7b2a84f9aeeb268df32b801ed4f479437
Daniel Lidstrom <dlidstrom@gmail.com>
2013-03-02 11:51:32 +0100

    In case we want to perform conflict detection in the future
    The problem seems to be that of handling the document session.
    When a ConcurrencyException is thrown it means the document
    session is invalid and we need a new one. So for this to happen
    it seems that the event store needs to manage the document session.
    That complicates things a bit, obviously. I'll leave it be for now.

---
+++ EventStoreLite.Test/ConflictDetectionTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using Raven.Client;
+
+namespace EventStoreLite.Test
+{
+    [TestFixture]
+    public class ConflictDetectionTest : TestBase
+    {
+        public class Initialized : Event {}
+        public class FirstActionOccurred : Event{}
+        public class SecondActionOccurred : Event { }
+        public class Aggregate : AggregateRoot
+        {
+            public Aggregate()
+            {
+                this.ApplyChange(new Initialized());
+            }
+            public Aggregate FirstAction()
+            {
+                this.ApplyChange(new FirstActionOccurred());
+                return this;
+            }
+            public Aggregate SecondAction()
+            {
+                this.ApplyChange(new SecondActionOccurred());
+                return this;
+            }
+        }
+
+        [Test, Ignore]
+        public void MergesEvents()
+        {
+            // Arrange
+            var container = CreateContainer();
+            var eventStore = container.Resolve<EventStore>();
+            var documentStore = container.Resolve<IDocumentStore>();
+
+            // store base aggregate
+            var aggregate = new Aggregate();
+            using (var documentSession = documentStore.OpenSession())
+            {
+                var eventStoreSession = eventStore.OpenSession(documentStore, documentSession);
+                eventStoreSession.Store(aggregate);
+                eventStoreSession.SaveChanges();
+            }
+
+            using (var firstSession = documentStore.OpenSession())
+            using (var secondSession = documentStore.OpenSession())
+            {
+                // use optimistic concurrency
+                // this will enable merging of events
+                secondSession.Advanced.UseOptimisticConcurrency = true;
+
+                var firstEventStoreSession = eventStore.OpenSession(documentStore, firstSession);
+
+                // Act
+
+                // make change to left side
+                firstEventStoreSession.Load<Aggregate>(aggregate.Id).FirstAction();
+
+                var secondEventStoreSession = eventStore.OpenSession(documentStore, secondSession);
+
+                // make change to right side
+                secondEventStoreSession.Load<Aggregate>(aggregate.Id).SecondAction();
+
+                firstEventStoreSession.SaveChanges();
+                secondEventStoreSession.SaveChanges();
+            }
+
+            // Assert
+
+            // expect merged result
+            using (var documentSession = documentStore.OpenSession())
+            {
+                var eventStoreSession = eventStore.OpenSession(documentStore, documentSession);
+                var mergedAggregate = eventStoreSession.Load<Aggregate>(aggregate.Id);
+                // Assert.That(mergedAggregate.GetHistory().Length, Is.EqualTo(3));
+            }
+        }
+    }
+}
